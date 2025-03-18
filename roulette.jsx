4ddde93e86c5b48e272696a33d66f796899bdfd6
Dionyzyzz <150077806+Dionyzyzz@users.noreply.github.com>
2025-03-18 15:27:48 +0100

    zfe
    ef

--- roulette.jsx
+++ roulette.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import 'roulette.css';
+import'roulette.css';
 
 const Roulette = () => {
   const [winningSpin, setWinningSpin] = useState(null);
